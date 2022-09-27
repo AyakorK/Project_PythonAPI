a3bcb76b730a25848b3860c37497ef48f5e36807
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-27 10:07:56 +0200

    Adding in a way a lot of types that are available in typing

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import List, Set, Tuple, Dict, Union
+
 
 app = FastAPI()
 
@@ -18,7 +20,14 @@ async def say_hello(name: str, q: str = None):
 @app.get("/hello/{first_name}/{last_name}/{age}")
 def get_full_name(first_name: str, last_name: str, age: int):
     full_name = first_name.title() + " " + last_name.title()
-    return {"full_name": full_name, "age": age}
+    name_age = "Hello " + full_name + " you are " + str(age) + " years old"
+    return name_age
 print(get_full_name("john", "doe", 30))
+
+def process_items(items: List[str], items_t: Tuple[int, int, str], items_s: Set[bytes], items_d: Dict[str, str], item_u: Union[int, str]):
+    return {"items": items, "items_t": items_t, "items_s": items_s, "items_d": items_d, "item_u": item_u}
+print(process_items(["a", "b", "c"], (1, 2, "3"), {b"1", b"2", b"3"}, {"a": "b", "c": "d"}, 1))
+
+
 
 
