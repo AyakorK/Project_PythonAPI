7e06ba2cd4deaf1089ec58f292285c72104bd5ef
AxelPerrin <91256042+AxelPerrin@users.noreply.github.com>
2022-09-27 16:28:18 +0200

    search_products_by_id

--- Project/main.py
+++ Project/main.py
@@ -2,14 +2,20 @@ from fastapi import FastAPI
 import json
 
 app = FastAPI()
-
 data = json.load(open("Project/db.json"))
 
 @app.get("/")
 async def root():
     return data
 
+@app.get("/{products}")
+async def root():
+    return data["products"]
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/product/{products_id}")
+async def get_products_by_id(products_id:int):
+    for products in data["products"]:
+        if products["id"] == products_id:
+            return products
+
+
