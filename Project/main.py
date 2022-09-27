50de3fa1e28bcc83b426db337798ec76dd96b9cd
ThomasCast3 <91255859+ThomasCast3@users.noreply.github.com>
2022-09-27 13:29:32 +0200

    exo tuto user guide path param

--- Project/main.py
+++ Project/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel
+from enum import Enum
 
 
 app = FastAPI()
@@ -11,10 +12,45 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id:int):
+    return{"item_id":item_id}
+
+@app.get("users/me")
+async def read_user_me():
+    return{"user_id":"the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id:str):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet :
+        return {"model_name":model_name,"message":"Deep Learning FTW!"}
+    if model_name.value == "lenet" :
+        return {"model_name":model_name,"message":"LeCNN all the images"}
+    return{"model_name":model_name,"message":"Have some residuals"}
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
 
 def get_full_name(first_name:str,last_name:str):
     full_name = first_name.title() + " " + last_name.title()
@@ -63,6 +99,3 @@ external_data ={
     "friends": [1,"2",b"3"],
 }
 user = User(**external_data)
-
-print(user)
-print(user.id)
