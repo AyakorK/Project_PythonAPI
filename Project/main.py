44660735d11eb5e94327ca32a41a3d8d17d889a3
Lucas Goncalves <lucasgoncalves@free.fr>
2022-09-27 11:44:11 +0200

    Adding item list

--- Project/main.py
+++ Project/main.py
@@ -3,6 +3,8 @@ from typing import Union
 from pydantic import BaseModel
 from enum import Enum
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
@@ -24,6 +26,11 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
@@ -39,4 +46,7 @@ async def get_model(model_name: ModelName):
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+
 
