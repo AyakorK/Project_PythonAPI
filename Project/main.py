12a3f2d486f06453e86cae36939325e3d1d16744
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-27 09:52:41 +0200

    Adding ages management

--- Project/main.py
+++ Project/main.py
@@ -15,10 +15,10 @@ async def root():
 async def say_hello(name: str, q: str = None):
     return {"message": f"Hello {name} - {q}"}
 
-@app.get("/hello/{first_name}/{last_name}")
-def get_full_name(first_name, last_name):
+@app.get("/hello/{first_name}/{last_name}/{age}")
+def get_full_name(first_name: str, last_name: str, age: int):
     full_name = first_name.title() + " " + last_name.title()
-    return {"full_name": full_name}
-print(get_full_name("john", "doe"))
+    return {"full_name": full_name, "age": age}
+print(get_full_name("john", "doe", 30))
 
 
