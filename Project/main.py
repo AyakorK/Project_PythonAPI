77596c741c04eb14cdaec0fc11a315deef525cb6
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-27 10:33:46 +0200

    Adding a new class User using Base Model and creating a new users with few items

--- Project/main.py
+++ Project/main.py
@@ -1,15 +1,34 @@
 from fastapi import FastAPI
-#from typing import List, Set, Tuple, Dict, Union
-from typing import Optional
+from typing import List, Union,Optional
+from datetime import datetime
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item:
-    def __init__(self, name: str, price: float, is_offer: Optional[bool] = None):
-        self.name = name
-        self.price = price
-        self.is_offer = is_offer
+class Item(BaseModel):
+        name: str
+        price: float
+        is_offer: Optional[bool] = None
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+    id: int
+    signup_ts: Union[datetime, None] = None
+    items: List[Item] = []
+
+external_data = {
+    "username": "johndoe",
+    "full_name": "John Doe",
+    "id": "123",
+    "signup_ts": datetime(2017, 6, 19, 11, 4, 5),
+    "items": [
+        {"name": "Foo", "price": 50.2},
+        {"name": "Bar", "price": 62, "is_offer": False},
+        {"name": "Baz", "price": 50.2, "is_offer": True},
+    ],
+}
 
 
 
@@ -45,6 +64,15 @@ print(get_full_name("john", "doe", 30))
 def describe_item(item: Item):
     return {"item_name": item.name, "item_price": item.price, "item_offer": item.is_offer}
 print(describe_item(Item(name="Foo", price=50.2)))
+
+user = User(**external_data)
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    if user_id == user.id:
+        return user
+    return user
+
 
 
 # def process_items(items: List[str], items_t: Tuple[int, int, str], items_s: Set[bytes], items_d: Dict[str, str], item_u: Union[int, str]):
