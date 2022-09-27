361f1b4b7fe9299bffc09b0cf869d7db8982b60c
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-27 09:48:10 +0200

    Adding a new root to search for the name of someone and print it in the console to verify it works

--- Project/main.py
+++ Project/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -11,3 +14,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str, q: str = None):
     return {"message": f"Hello {name} - {q}"}
+
+@app.get("/hello/{first_name}/{last_name}")
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return {"full_name": full_name}
+print(get_full_name("john", "doe"))
+
+
