a4591c859bb2838925b4483389274e1283689e6e
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-27 15:06:01 +0200

    Get users from database and you can search for one particular user

--- Project/main.py
+++ Project/main.py
@@ -9,6 +9,18 @@ data = json.load(open("Project/db.json"))
 async def root():
     return data
 
+@app.get("/users")
+async def get_users():
+    if data["users"]:
+        return data["users"]
+    return {"message": "No users found"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    for user in data["users"]:
+        if user["id"] == user_id:
+            return user
+    return {"error": "User not found"}
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
